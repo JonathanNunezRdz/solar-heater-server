b8de7d16b0088ff827a067b2954219f2cca97af8
JonathanNunezRdz <jonathannunezr1@gmail.com>
2021-05-30 13:48:54 -0500

    Set up alternative mpu5060 data extraction

--- gyro_mk2.py
+++ gyro_mk2.py
@@ -3,12 +3,67 @@ from mpu6050 import mpu6050
 import csv
 
 sensor = mpu6050(0x68)
-sensor.set_gyro_range(sensor.GYRO_RANGE_2000DEG)
+# sensor.set_gyro_range(sensor.GYRO_RANGE_2000DEG)
 # sensor.set_accel_range(sensor.ACCEL_RANGE_16G)
-# print('accel range', sensor.read_accel_range())
-print('gyro_range', sensor.read_gyro_range())
 # start_time = time()
 
+accel_range = {
+    '2': sensor.ACCEL_RANGE_2G,
+    '4': sensor.ACCEL_RANGE_4G,
+    '8': sensor.ACCEL_RANGE_8G,
+    '16': sensor.ACCEL_RANGE_16G,
+}
+
+gyro_range = {
+    '250': sensor.GYRO_RANGE_250DEG,
+    '500': sensor.GYRO_RANGE_500DEG,
+    '1000': sensor.GYRO_RANGE_1000DEG,
+    '2000': sensor.GYRO_RANGE_2000DEG,
+}
+
+def get_acceleration():
+    print('accel range', sensor.read_accel_range())
+    return sensor.get_accel_data()
+
+def get_gyroscope():
+    print('gyro range', sensor.read_gyro_range())
+    return sensor.get_gyro_data()
+
+def set_acceleration(range='2'):
+    sensor.set_accel_range(range)
+
+def main():
+    loop = True
+    while(loop):
+        print('Option A - acceleration')
+        print('Option B - gyroscope')
+        print('Optino C - set ranges')
+        option = input()
+        if option == 'A': print(get_acceleration())
+        elif option == 'B': print(get_gyroscope())
+        elif option == 'C':
+            print('Option A - set acceleration range')
+            print('Option B - set gyroscope range')
+            option = input()
+            if option == 'A':
+                print('Option 2 - 2G')
+                print('Option 4 - 4G')
+                print('Option 8 - 8G')
+                print('Option 16 - 16G')
+                option = input()
+                if option in ['2', '4', '8', '16']: set_acceleration(option)
+                else: exit(2)
+            elif option == 'B':
+                print('Option 250 - 250DEG')
+                print('Option 500 - 500DEG')
+                print('Option 1000 - 1000DEG')
+                print('Option 2000 - 2000DEG')
+                option = input()
+                if option in ['250', '500', '1000', '2000']: set_acceleration(option)
+                else: exit(2)
+        print('Do you want to continue - (y/n)')
+        option = input()
+        if (option == 'n'): loop = False
 
 # with open('module_data.csv', mode='w') as csv_file:
 #     fieldnames = ['time_lapsed', 'accel_x', 'accel_y', 'accel_z', 'gyro_x', 'gyro_y', 'gyro_z']
