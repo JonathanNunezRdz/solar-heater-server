3e710c028507eccf8d44b8c247c7ce815f8d9fe0
JonathanNunezRdz <jonathannunezr1@gmail.com>
2021-05-29 20:22:11 -0500

    Added gyro_mk2.py

---
+++ gyro_mk2.py
@@ -0,0 +1,27 @@
+from time import sleep, time
+from mpu6050 import mpu6050
+import csv
+
+sensor = mpu6050(0x68)
+# sensor.set_gyro_range(sensor.GYRO_RANGE_500DEG)
+print('accel range', sensor.read_accel_range())
+# start_time = time()
+
+
+# with open('module_data.csv', mode='w') as csv_file:
+#     fieldnames = ['time_lapsed', 'accel_x', 'accel_y', 'accel_z', 'gyro_x', 'gyro_y', 'gyro_z']
+#     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+#     writer.writeheader()
+#     while True:
+#         # do something
+#         accel_data = sensor.get_accel_data()
+#         gyro_data = sensor.get_gyro_data()
+#         time_lapsed = time() - start_time
+#         accel_x = accel_data['x']
+#         accel_y = accel_data['y']
+#         accel_z = accel_data['z']
+#         gyro_x = gyro_data['x']
+#         gyro_y = gyro_data['y']
+#         gyro_z = gyro_data['z']
+#         writer.writerow({'time_lapsed': time_lapsed, 'accel_x': accel_x, 'accel_y': accel_y, 'accel_z': accel_z, 'gyro_x': gyro_x, 'gyro_y': gyro_y, 'gyro_z': gyro_z })
+#         sleep(1)
